c1062bf3de34489d0a82e4551a96c3703aaf9135
Tom Weston <weston.tom@gmail.com>
2018-05-24 15:51:13 +0100

    adds add secret command

---
+++ cmd/add.go
@@ -0,0 +1,101 @@
+// Copyright © 2018 The GΛTEKEEPER Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
+	"github.com/spf13/cobra"
+)
+
+// addCmd represents the create command
+
+var name string
+var addRegion string
+var value string
+var description string
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add a new secret to AWS Secrets Manager",
+	Long: `Add a new secret to AWS Secrets Manager.
+
+	Access to secrets is granted via fine-grained 
+	AWS Identity and Access Management (IAM) policies.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		addSecret(cmd)
+
+	},
+}
+
+func addSecret(cmd *cobra.Command) {
+
+	region := cmd.Flag("region").Value.String()
+	description := cmd.Flag("description").Value.String()
+	name := cmd.Flag("name").Value.String()
+	value := cmd.Flag("value").Value.String()
+	svc := secretsmanager.New(session.New(), aws.NewConfig().WithRegion(region))
+	input := &secretsmanager.CreateSecretInput{
+		Description:  aws.String(description),
+		Name:         aws.String(name),
+		SecretString: aws.String(value),
+	}
+
+	result, err := svc.CreateSecret(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case secretsmanager.ErrCodeInvalidParameterException:
+				fmt.Println(secretsmanager.ErrCodeInvalidParameterException, aerr.Error())
+			case secretsmanager.ErrCodeInvalidRequestException:
+				fmt.Println(secretsmanager.ErrCodeInvalidRequestException, aerr.Error())
+			case secretsmanager.ErrCodeLimitExceededException:
+				fmt.Println(secretsmanager.ErrCodeLimitExceededException, aerr.Error())
+			case secretsmanager.ErrCodeEncryptionFailure:
+				fmt.Println(secretsmanager.ErrCodeEncryptionFailure, aerr.Error())
+			case secretsmanager.ErrCodeResourceExistsException:
+				fmt.Println(secretsmanager.ErrCodeResourceExistsException, aerr.Error())
+			case secretsmanager.ErrCodeResourceNotFoundException:
+				fmt.Println(secretsmanager.ErrCodeResourceNotFoundException, aerr.Error())
+			case secretsmanager.ErrCodeMalformedPolicyDocumentException:
+				fmt.Println(secretsmanager.ErrCodeMalformedPolicyDocumentException, aerr.Error())
+			case secretsmanager.ErrCodeInternalServiceError:
+				fmt.Println(secretsmanager.ErrCodeInternalServiceError, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	fmt.Println(result)
+}
+
+func init() {
+
+	addCmd.Flags().StringVarP(&addRegion, "region", "r", "", "The region the secret is stored in")
+	addCmd.Flags().StringVarP(&name, "name", "n", "", "The name of the secret")
+	addCmd.Flags().StringVarP(&value, "value", "v", "", "The value of the secret")
+	addCmd.Flags().StringVarP(&description, "description", "d", "", "The description of the secret")
+	rootCmd.AddCommand(addCmd)
+}
