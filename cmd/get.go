cea12dd547092326820892752d366d69b3b64ac7
Tom Weston <weston.tom@gmail.com>
2018-07-05 11:40:41 +0100

    adds key picking and all flags to get command

--- cmd/get.go
+++ cmd/get.go
@@ -15,6 +15,7 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -28,6 +29,10 @@ import (
 
 var gSecret string
 var gRegion string
+var gKey string
+var gAll bool
+
+// var gKey string
 var getCmd = &cobra.Command{
 	Use:   "get",
 	Short: "Get the value of a secret stored AWS Secrets Manager",
@@ -36,14 +41,20 @@ var getCmd = &cobra.Command{
 	Access to secrets is granted via fine-grained 
 	AWS Identity and Access Management (IAM) policies.`,
 	Run: func(cmd *cobra.Command, args []string) {
-
 		getSecret(cmd)
-
 	},
 }
 
-func getSecret(cmd *cobra.Command) {
+func init() {
+	getCmd.Flags().StringVarP(&gRegion, "region", "r", "", "The region the secret is stored in")
+	getCmd.Flags().StringVarP(&gSecret, "secret", "s", "", "The name of the secret stored in AWS Secrets Manager")
+	getCmd.Flags().BoolVarP(&gAll, "all", "a", false, "Display all details for secret")
+	getCmd.Flags().StringVarP(&gKey, "key", "k", "", "Key of item in secret string")
+	rootCmd.AddCommand(getCmd)
+}
 
+func getSecret(cmd *cobra.Command) {
+	key := cmd.Flag("key").Value.String()
 	region := cmd.Flag("region").Value.String()
 	secret := cmd.Flag("secret").Value.String()
 	svc := secretsmanager.New(session.New(), aws.NewConfig().WithRegion(region))
@@ -78,13 +89,28 @@ func getSecret(cmd *cobra.Command) {
 		return
 	}
 
-	fmt.Println(result)
-
+	displayResult(result, key)
 }
 
-func init() {
+func displayResult(result *secretsmanager.GetSecretValueOutput, key string) {
+	if gAll {
+		fmt.Println(result)
+	} else {
+		var secretResult map[string]interface{}
+		json.Unmarshal([]byte(*result.SecretString), &secretResult)
 
-	getCmd.Flags().StringVarP(&gRegion, "region", "r", "", "The region the secret is stored in")
-	getCmd.Flags().StringVarP(&gSecret, "secret", "s", "", "The name of the secret stored in AWS Secrets Manager")
-	rootCmd.AddCommand(getCmd)
+		// If a string key provided try and return the value of the key
+		if key != "" {
+			value, exists := secretResult[key]
+
+			// If key exists return value, else respond with invalid key message
+			if exists {
+				fmt.Println(value)
+			} else {
+				fmt.Println("Key doesn't exist")
+			}
+		} else {
+			fmt.Println(*result.SecretString)
+		}
+	}
 }
